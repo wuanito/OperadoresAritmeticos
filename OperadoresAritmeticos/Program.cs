1492db4ef54876abe51d4ff57ded4d175387c189
Juan Frauca <juancore97@gmail.com>
2026-03-13 09:08:09 +0100

    Fin Aritmeticos

--- OperadoresAritmeticos/Program.cs
+++ OperadoresAritmeticos/Program.cs
@@ -10,6 +10,16 @@ namespace OperadoresAritmeticos
     {
         static void Main(string[] args)
         {
+
+            byte num1 = 20;
+            byte num2 = 10;
+
+            int suma = num1 + num2;
+            int resta = num1 - num2;
+            int multiplicacion = num1 * num2;
+            int division = num1 / num2;
+            Console.WriteLine("La suma es : {0} y la resta es : {1}, la multiplicacion es {2} la division es {3}", suma,resta,multiplicacion,division);
+            Console.ReadLine();
         }
     }
 }
